a5d6f243c781658bd814845a97df9db454296448
qwee0328 <qwee0328@naver.com>
2023-12-07 17:08:53 +0900

    로그인 페이지 ui 완성

--- src/pages/MemberLogin/Login.js
+++ src/pages/MemberLogin/Login.js
@@ -1,7 +1,37 @@
 import style from "./Login.module.css";
+import PurpleRectangleBtn from "../../components/PurpleRectangleBtn/PurpleRectangleBtn";
 
 const Login = () => {
-  return <div>ㅅㄷㄴㅅ</div>;
+  return (
+    <div className={style.loginBox}>
+      <div className={style.logo}>
+        <span>L</span>OG<span>I</span>N
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" id="saveId" />
+          <label htmlFor="saveId">아이디 기억하기</label>
+        </div>
+        <div>
+          <div>회원가입</div>
+          <div>아이디/비밀번호 찾기</div>
+        </div>
+      </div>
+      <div>
+        <PurpleRectangleBtn
+          title={"로그인"}
+          width={150}
+          heightPadding={10}
+        ></PurpleRectangleBtn>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
