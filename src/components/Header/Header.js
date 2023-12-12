76a8ab9ba13e71038d2024851d2fd18fb1750227
qwee0328 <qwee0328@naver.com>
2023-12-12 11:33:59 +0900

    로그인 및 로그인  시 header 변경

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import style from "./Header.module.css";
 import PurpleRoundBtn from "../PurpleRoundBtn/PurpleRoundBtn";
+
 import { Link } from "react-router-dom";
-import { MenuContext } from "../../App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { MenuContext } from "../../App";
+import { LoginContext } from "../../App";
 
 const Header = () => {
   const [profileUrl, setProfileUrl] = useState("/assets/basicProfile.png");
+  const { loginId } = useContext(LoginContext);
+
+  useEffect(() => {
+    console.log(loginId);
+  }, []);
+
   return (
     <div className={style.header}>
       <div className={style.header__pcSize}>
@@ -23,18 +31,20 @@ const Header = () => {
             <div className={style.navi__conf}>파티 찾기</div>
             <div className={style.navi__conf}>게시판</div>
             <div className={style.navi__conf}>자주 묻는 질문</div>
-            <Link to="login">
-              <PurpleRoundBtn
-                title={"로그인"}
-                activation={true}
-              ></PurpleRoundBtn>
-            </Link>
+            {loginId === "" ? (
+              <Link to="login">
+                <PurpleRoundBtn
+                  title={"로그인"}
+                  activation={true}
+                ></PurpleRoundBtn>
+              </Link>
+            ) : (
+              <div className={style.menu__user}>
+                <FontAwesomeIcon icon={faBell} />
+                <img src={profileUrl} alt="" className={style.profileImg} />
+              </div>
+            )}
           </div>
-
-          {/* <div className={style.menu__user}>
-            <FontAwesomeIcon icon={faBell} />
-            <img src={profileUrl} alt="" className={style.profileImg} />
-          </div> */}
         </div>
         <div className={style.loginBtn}>
           <Link to="login">
--- src/pages/MemberLogin/Login/Login.js
+++ src/pages/MemberLogin/Login/Login.js
@@ -1,8 +1,45 @@
 import style from "./Login.module.css";
 import PurpleRectangleBtn from "../../../components/PurpleRectangleBtn/PurpleRectangleBtn";
-import { Link } from "react-router-dom";
+import axios from "axios";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { LoginContext } from "../../../App";
 
 const Login = () => {
+  const [user, setUser] = useState({ id: "", pw: "" });
+  const { setLoginId } = useContext(LoginContext);
+  const navi = useNavigate();
+
+  // user State 값 채우기
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleLogin = () => {
+    console.log(user.id);
+    if (user.id !== "" && user.pw !== "") {
+      const formData = new FormData();
+      formData.append("id", user.id);
+      formData.append("pw", user.pw);
+
+      axios.post("/api/member/login", formData).then((resp) => {
+        console.log("로그인 성공");
+        console.log(resp);
+        if (resp.statusText === "OK") {
+          setLoginId(user.id);
+          setUser({ id: "", pw: "" });
+          navi(-1);
+        } else if (resp.statusText === "UNAUTHORIZED") {
+          alert("아이디 혹은 비밀번호가 일치하지 않습니다.");
+          setUser({ id: "", pw: "" });
+        }
+      });
+    } else {
+      alert("아이디, 비밀번호를 모두 입력해주세요.");
+    }
+  };
   return (
     <div className={style.loginBox}>
       <div className={style.logo}>
@@ -10,10 +47,22 @@ const Login = () => {
       </div>
       <div className={style.loginInputBox}>
         <div>
-          <input type="text" />
+          <input
+            type="text"
+            name="id"
+            placeholder="아이디"
+            onChange={handleChange}
+            value={user.id}
+          />
         </div>
         <div>
-          <input type="password" />
+          <input
+            type="password"
+            name="pw"
+            placeholder="비밀번호"
+            onChange={handleChange}
+            value={user.pw}
+          />
         </div>
       </div>
 
@@ -34,6 +83,7 @@ const Login = () => {
           title={"로그인"}
           width={400}
           heightPadding={15}
+          onClick={handleLogin}
         ></PurpleRectangleBtn>
       </div>
     </div>
