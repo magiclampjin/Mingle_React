9ce0a48fe87d7e86bcceecdfeff57330dcc73b01
Jeen <eyungenius@gmail.com>
2023-12-13 21:11:34 +0900

    party create page 파일 생성

--- src/pages/PartyCreate/PartyCreateList/ServiceInfoModal/ServiceInfoModal.js
+++ src/pages/PartyCreate/PartyCreateList/ServiceInfoModal/ServiceInfoModal.js
@@ -29,7 +29,7 @@ const ServiceInfoModal = ({ isOpen, onRequestClose, contentLabel, selectService,
     // 요금 안내 확인 후 파티 생성창으로 이동하는 버튼 클릭 (다음 버튼)
     const handleNext = () => {
         if(isChked){
-            navi("/");
+            navi("/party/PartyCreatePage");
         }
     }
 
