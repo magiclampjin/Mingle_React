40e9918a064bc5a7c747123c1593136cb865fe8a
changgyun kim <kiki981011@gmail.com>
2023-12-19 00:55:50 +0900

    temporary commit

--- src/pages/Board/Post/Post.js
+++ src/pages/Board/Post/Post.js
@@ -1,41 +1,29 @@
 import styles from "./Post.module.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
+import { useState,useEffect, useContext } from "react";
 import { LoginContext } from "../../../App";
 
-const Post = () => {
-    const { postId } = useParams();
-    const { loginId } = useContext(LoginContext);
-    const [post, setPost] = useState(null);
+const Post = () =>{
 
-    useEffect(() => {
-        axios.get(`/api/post/${postId}`)
-            .then(resp => {
-                setPost(resp.data);
-            })
-            .catch(error => {
-                console.error("error", error);
-            });
-    }, [postId]);
+    const{postId} = useParams();
+    const{loginId} = useContext(LoginContext);
 
-    if (!post) {
-        return <div>Loading...</div>;
-    }
+    useEffect(()=>{
+        axios.get(`/api/post/${postId}`).then(resp => {
+            console.log(resp);
+        }).catch(error=>{
+            console.error("error",error);
+        });
 
-    return (
+    }, [postId])
+
+    
+
+    return(
+
         <div className={styles.board}>
-            <div className={styles.postHeader}>
-                <h1 className={styles.postTitle}>{post.title}</h1>
-                <div className={styles.postInfo}>
-                    <span>{post.author}</span>
-                    <span>{new Date(post.createdAt).toLocaleDateString()}</span>
-                </div>
-            </div>
-            <div className={styles.postContent}>
-                <p>{post.content}</p>
-            </div>
-            {/* 추가적인 포스트 내용이나 기능들을 여기에 구현 */}
+            작성중....
         </div>
     );
 };
