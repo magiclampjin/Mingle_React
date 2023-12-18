90544c355665646b75b4556d2f3c80720ef22bce
Jeen <eyungenius@gmail.com>
2023-12-18 15:34:33 +0900

    파티 자동매칭 / 파티 직접 생성 버튼 및 파티만들기로 이동하는 기능 구현

--- src/pages/Party/PartyJoin/PartyList/PartyList.js
+++ src/pages/Party/PartyJoin/PartyList/PartyList.js
@@ -1,8 +1,8 @@
 import style from "./PartyList.module.css"
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Navigate, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWonSign } from "@fortawesome/free-solid-svg-icons";
+import { faWonSign, faCirclePlus, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
 import axios from "axios";
 import LoadingSpinner from "../../../../components/LoadingSpinner/LoadingSpinner";
@@ -14,6 +14,7 @@ const PartyList = () => {
     const [service, setService] = useState();
     const [isLoading, setLoading] = useState(false);
     const [price, setPrice] = useState();
+    const navi = useNavigate();
 
     useEffect(()=>{
         setLoading(true);
@@ -23,7 +24,6 @@ const PartyList = () => {
             setPrice(formatNumber(Math.ceil((resp.data.price)/(resp.data.maxPeopleCount))+1000));
             axios.get(`/api/party/getPartyList/${selectService}`).then(resp=>{
                 setPartyList(resp.data);
-                console.log(resp.data);
                 setLoading(false);
             }).catch(()=>{
                 setLoading(false);
@@ -77,6 +77,16 @@ const PartyList = () => {
         endDate.setMonth(endDate.getMonth()+period);
         return endDate.toISOString().slice(0,10);
     }
+
+    // 파티 만들기 페이지로 이동
+    const goCreateParty = (e) =>{
+        const contentElement = e.currentTarget;
+        const clickedElement = e.target; 
+        
+        if (clickedElement === contentElement || contentElement.contains(clickedElement)) {
+            navi("/party/partyCreate");
+        }
+    };
     
     return(
         <div className={style.body}>
@@ -99,6 +109,7 @@ const PartyList = () => {
                     <div className={`${style.partyList}`}>
                         <div className={style.subTitle}>파티 검색 결과</div>
                         {
+                            // 가입 가능한 파티가 있는 경우
                             partyList.length>0?partyList.map((e,i)=>( 
                                 <div key={i} className={`${style.party}`} data-id={e.id}>
                                     <div className={`${style.partyTop} ${style.dflex}`}>
@@ -112,11 +123,45 @@ const PartyList = () => {
                                     </div>
                                 </div>
                             )):
+                            // 가입 가능한 파티가 없는 경우
                             <div className={style.empty}>
                                 <div className={`${style.emptyIcon} ${style.centerAlign}`}><FontAwesomeIcon icon={faFaceSadTear}/></div>
                                 <div className={`${style.emptyTxt} ${style.centerAlign}`}>비어있는 파티가 없어요.</div>
                             </div>
                         }
+                        <div className={`${style.others}`}>
+                            <div className={`${style.title} ${style.othersTitle}`}>원하는 파티가 없다면</div>
+                            <div className={`${style.party} ${style.dflex}`}>
+                                <div className={style.left}>
+                                    <div className={`${style.title}`}>
+                                        파티 매칭하기   
+                                    </div>
+                                    <div className={`${style.othersExplain}`}>
+                                        원하는 조건의 파티가 열리면 바로 매칭돼요 !
+                                    </div>
+                                </div>
+                                <div className={style.right}>
+                                    <div class={style.othersIcon}>
+                                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={`${style.party} ${style.dflex}`} onClick={goCreateParty}>
+                                <div className={style.left}>
+                                    <div className={`${style.title}`}>
+                                        파티 만들기   
+                                    </div>
+                                    <div className={`${style.othersExplain}`}>
+                                        원하는 조건이 있다면 직접 만들어 보세요 !
+                                    </div>
+                                </div>
+                                <div className={style.right}>
+                                    <div class={style.othersIcon}>
+                                        <FontAwesomeIcon icon={faCirclePlus} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </>
             :null
