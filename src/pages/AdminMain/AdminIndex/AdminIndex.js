4435d36826645c250996062fbb490b95a1867877
anuygnaj <anuygnaj@naver.com>
2023-12-19 16:35:14 +0900

    관리자 권한일 때만 관리자 페이지 접근 가능

--- src/pages/AdminMain/AdminIndex/AdminIndex.js
+++ src/pages/AdminMain/AdminIndex/AdminIndex.js
@@ -6,29 +6,65 @@ import axios from "axios";
 import MemberManageBox from '../components/MemberManageBox/MemberManageBox';
 import NoticeBoardBox from '../components/NoticeBoardBox/NoticeBoardBox';
 import StatisticsBox from "../components/StatisticsBox/StatisticsBox";
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
 
 const AdminIndex = () => {
 
     const [authority, setAuthority] = useState();
+
+    // 선택한 서비스 정보 불러오기 로딩
+    const [isServiceLoading, setServiceLoading] = useState(false);
     
     useEffect(() => {
+        setServiceLoading(true);
+        
         axios.get("api/member/isAdmin").then(resp => {
             setAuthority(resp.data);
+            setServiceLoading(false);
+        }).catch(() => {
+            setServiceLoading(false);
         })
     }, []);
-
-    if(!authority) {
-        return <Navigate to="/denied" />
-    }
+    
 
     return (
-        <div className={style.background}>
-            <div className={style.body}>
-                <MemberManageBox></MemberManageBox>
-                <NoticeBoardBox></NoticeBoardBox>
-                <StatisticsBox></StatisticsBox>
-            </div>
-        </div>
+        <>
+            {isServiceLoading || authority===undefined ? (
+                <LoadingSpinner />
+            ) : (
+                <>
+                    {authority ? (
+                        <div className={style.background}>
+                            <div className={style.body}>
+                                <MemberManageBox />
+                                <NoticeBoardBox />
+                                <StatisticsBox />
+                            </div>
+                        </div>
+                    ) : (
+                        <Navigate to="/denied" />
+                    )}
+                </>
+            )}
+        </>
+        // isServiceLoading ? (
+        //     <LoadingSpinner />
+        // ) : (
+        //     <>
+        //     {console.log(authority)}
+        //     authority ? (
+        //         <div className={style.background}>
+        //         <div className={style.body}>
+        //             <MemberManageBox />
+        //             <NoticeBoardBox />
+        //             <StatisticsBox />
+        //         </div>
+        //         </div>
+        //     ) : (
+        //         <Navigate to="/denied" />
+        //     )
+        //     </>
+        // )
     );
 }
 
