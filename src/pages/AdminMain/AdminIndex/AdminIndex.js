c5bc8b04b7ae9fadd2ee82501718f55a57aeb9eb
anuygnaj <anuygnaj@naver.com>
2023-12-19 11:51:14 +0900

    관리자 권한 없을 시 denied로 이동

--- src/pages/AdminMain/AdminIndex/AdminIndex.js
+++ src/pages/AdminMain/AdminIndex/AdminIndex.js
@@ -1,10 +1,26 @@
 import style from "../AdminMain.module.css"
+import { useEffect, useState } from "react";
+import { Navigate } from 'react-router-dom';
+import axios from "axios";
 
 import MemberManageBox from '../components/MemberManageBox/MemberManageBox';
 import NoticeBoardBox from '../components/NoticeBoardBox/NoticeBoardBox';
 import StatisticsBox from "../components/StatisticsBox/StatisticsBox";
 
 const AdminIndex = () => {
+
+    const [authority, setAuthority] = useState();
+    
+    useEffect(() => {
+        axios.get("api/member/isAdmin").then(resp => {
+            setAuthority(resp.data);
+        })
+    }, []);
+
+    if(!authority) {
+        return <Navigate to="/denied" />
+    }
+
     return (
         <div className={style.background}>
             <div className={style.body}>
