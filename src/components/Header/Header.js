7f621f7c4064276ba0cb535155694e0e2a1ec1d0
anuygnaj <anuygnaj@naver.com>
2023-12-24 23:37:55 +0900

    헤더/푸터 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -66,7 +66,7 @@ const Header = () => {
 
   // 자주 묻는 질문으로 이동
   const handleFAQClick = () => {
-    window.location.href = 'https://impossible-log-6dc.notion.site/4ac5ec788ca04f6ab7304dbb71891974?pvs=4';
+    window.open('https://impossible-log-6dc.notion.site/4ac5ec788ca04f6ab7304dbb71891974?pvs=4');
   }
 
   return (
