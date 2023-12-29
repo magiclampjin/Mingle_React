b27b428ce34d6ba4f3f9f4d7fdd6d39df94e51d1
Jeen <eyungenius@gmail.com>
2023-12-29 09:11:46 +0900

    파티 결과 모달창 구현중

--- src/pages/Party/MyParty/MyPartyInfo/PartyWithdrawalModal/PartyWithdrawalModal.js
+++ src/pages/Party/MyParty/MyPartyInfo/PartyWithdrawalModal/PartyWithdrawalModal.js
@@ -12,22 +12,23 @@ const PartyWithdrawalModal = ({isOpen, onRequestClose, width, height}) => {
             isOpen={isOpen}
             onRequestClose={onRequestClose}
             style={{
-            overlay: {
-                backgroundColor: "rgba(0, 0, 0, 0.5)"
-            },
-            content: {
-                position: 'fixed',
-                top: '50%',
-                left: '50%',
-                width: width+"px", // 모달의 가로 크기
-                height : height+"px", // 모달의 세로 크기
-                transform: 'translate(-50%, -50%)',
-                borderRadius: '20px'
-            },
+                overlay: {
+                    backgroundColor: "rgba(0, 0, 0, 0.5)"
+                },
+                content: {
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    width: width+"px", // 모달의 가로 크기
+                    height : height+"px", // 모달의 세로 크기
+                    transform: 'translate(-50%, -50%)',
+                    borderRadius: '20px'
+                },
             }}
         >
-         
-   
+            <div>
+                파티 탈퇴
+            </div>
         </Modal>
     );
 }
