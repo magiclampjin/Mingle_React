2de5631684a53ebd858a320abe3999c5b74c4566
Jeen <eyungenius@gmail.com>
2024-01-01 22:39:39 +0900

    파티 시간 정보 출력부분 수정 & 아이디/비번찾기 로그인 시 접근 막기

--- src/pages/Member/FindInfomation/FindInfomation.js
+++ src/pages/Member/FindInfomation/FindInfomation.js
@@ -1,9 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import FindType from "./FindType/FindType";
 import FindId from "./FindId/FindId";
 import FindPw from "./FindPw/FindPw";
+import { useContext, useEffect } from "react";
+import { LoginContext } from "../../../App";
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
 
 const FindInfomation = () => {
+
+  const { loginId, loginStatus } = useContext(LoginContext);
+  const navi = useNavigate();
+
+  useEffect(()=>{
+    if(loginStatus=="loading"){
+      <LoadingSpinner/>
+    }else{
+      if(loginId!==""){
+        navi("/")
+      }
+    }
+  },[loginId, loginStatus]);
+
   return (
     <Routes>
       <Route path="/" element={<FindType />}></Route>
